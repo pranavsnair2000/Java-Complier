660d817e8af3ee34cfe5864414e79d9656656fb4
Pranav <51502593+pranavsnair2000@users.noreply.github.com>
2021-05-14 21:34:58 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,55 @@
+
+
+/* comment line 1
+comment line 2
+*/
+
+//another comment
+
+import pack;
+
+public class test {
+	public static void main (String[] args) {
+		
+		//3+5;
+		
+		int a=2+3/4;
+		float f=2.3;
+		char c = 'w';
+		
+		int b;
+		
+		
+		int x;
+		x= b+2;
+		x++;
+		
+		
+		
+		
+		if (true) {
+			String str1="if";
+			
+		}
+		else if (a<5) { 
+			String str4="else if";
+		}
+		else { 
+			String str5="else";
+		}
+		
+		String str6="out";
+		
+		
+		int ff=0;
+		for(int i=5; i<10; i++) {
+			ff=ff+i;
+		}
+		
+		System.out.println("sample");
+	
+	}
+	
+}
+
+
